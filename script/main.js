0624d0b1813ce1223bb5f9558d04fe5e7608b61a
LucianoKlein <kleinersilver@gmail.com>
2018-01-13 03:27:17 +0800

    完成了画板的画笔橡皮擦选色功能

--- script/main.js
+++ script/main.js
@@ -2,7 +2,58 @@ var yyy = document.getElementById('canva');
 var context = yyy.getContext('2d');
 
 autoSetCanvasSize(yyy);
-listenToMouse(yyy);
+listenToUser(yyy);
+
+var eraserEnabled = false;
+brush.onclick = function() {
+  eraserEnabled = false;
+
+  brush.classList.add('active');
+  eraser.classList.remove('active');
+}
+
+eraser.onclick = function() {
+  eraserEnabled = true;
+
+  eraser.classList.add('active');
+  brush.classList.remove('active');
+}
+
+black.onclick = function() {
+  context.strokeStyle = 'black';
+  black.classList.add('active');
+
+  red.classList.remove('active');
+  green.classList.remove('active');
+  blue.classList.remove('active');
+}
+
+red.onclick = function() {
+  context.strokeStyle = 'red';
+  red.classList.add('active');
+
+  black.classList.remove('active');
+  green.classList.remove('active');
+  blue.classList.remove('active');
+}
+
+green.onclick = function() {
+  context.strokeStyle = 'green';
+  green.classList.add('active');
+
+  black.classList.remove('active');
+  red.classList.remove('active');
+  blue.classList.remove('active');
+}
+
+blue.onclick = function() {
+  context.strokeStyle = 'blue';
+  blue.classList.add('active');
+
+  black.classList.remove('active');
+  red.classList.remove('active');
+  green.classList.remove('active');
+}
 
 /***/
 function autoSetCanvasSize(canvas) {
@@ -19,21 +70,89 @@ function autoSetCanvasSize(canvas) {
   }
 }
 
-
-
-function listenToMouse(canvas) {
+function listenToUser(canvas) {
   var context = canvas.getContext('2d');
-  var usingEraser = false;
   var mousingdown = false;
   var lastPoint = {
     x: undefined,
     y: undefined
   };
 
-  yyy.onmousedown = function (a) {
+  canvas.addEventListener("touchstart", function (a) {
+    var x = a.touches[0].clientX;
+    var y = a.touches[0].clientY;
+    if (eraserEnabled) {
+      mousingdown = true;
+      context.clearRect(x, y, 10, 10);
+    } else {
+      mousingdown = true;
+      lastPoint = {
+        "x": x,
+        "y": y
+      };
+    }
+  });
+
+  canvas.addEventListener("touchmove", function (a) {
+    var x = a.touches[0].clientX;
+    var y = a.touches[0].clientY;
+    if (eraserEnabled) {
+      if (mousingdown) {
+        context.clearRect(x, y, 10, 10);
+      }
+    } else {
+      if (mousingdown) {
+        var newPoint = {
+          "x": x,
+          "y": y
+        };
+        drawCircle(x, y, 3);
+        drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y);
+        lastPoint = newPoint;
+      }
+    }
+
+    function drawLine(x1, y1, x2, y2) {
+      context.beginPath();
+      context.moveTo(x1, y1); // 起点
+      context.lineWidth = 5;
+      context.lineTo(x2, y2); // 终点
+      context.stroke();
+      context.closePath();
+    }
+    
+    function drawCircle(x, y, radius) {
+      context.beginPath();
+      context.fillStyle = context.strokeStyle;
+      context.arc(x, y, radius, 0, Math.PI * 2);
+      context.fill();
+    }
+  })
+
+  canvas.addEventListener("touchend", function () {
+    mousingdown = false;
+  })
+
+  /**
+   * 下边无法执行
+   
+  canvas.ontouchstart = function () {
+    console.log('开始摸我了');
+  }
+
+  canvas.ontouchmove = function () {
+    console.log('边摸边动');
+  }
+
+  canvas.ontouchend = function () {
+    console.log('摸完了');
+  }
+  */
+
+  canvas.onmousedown = function (a) {
     var x = a.clientX;
     var y = a.clientY;
-    if (usingEraser) {
+    if (eraserEnabled) {
       mousingdown = true;
       context.clearRect(x, y, 10, 10);
     } else {
@@ -45,10 +164,10 @@ function listenToMouse(canvas) {
     }
   }
 
-  yyy.onmousemove = function (a) {
+  canvas.onmousemove = function (a) {
     var x = a.clientX;
     var y = a.clientY;
-    if (usingEraser) {
+    if (eraserEnabled) {
       if (mousingdown) {
         context.clearRect(x, y, 10, 10);
       }
@@ -64,22 +183,17 @@ function listenToMouse(canvas) {
       }
     }
     eraser.onclick = function () {
-      usingEraser = true;
+      eraserEnabled = true;
       actions.className = 'actions x';
     }
 
     brush.onclick = function () {
-      usingEraser = false;
+      eraserEnabled = false;
       actions.className = 'actions';
-    }
-    
-    yyy.onmouseup = function () {
-      mousingdown = false;
     }
 
     function drawLine(x1, y1, x2, y2) {
       context.beginPath();
-      context.strokeStyle = 'black';
       context.moveTo(x1, y1); // 起点
       context.lineWidth = 5;
       context.lineTo(x2, y2); // 终点
@@ -88,11 +202,15 @@ function listenToMouse(canvas) {
     }
 
     function drawCircle(x, y, radius) {
+      context.fillStyle = context.strokeStyle;
       context.beginPath();
-      context.fillStyle = 'black';
       context.arc(x, y, radius, 0, Math.PI * 2);
       context.fill();
     }
+  }
+
+  canvas.onmouseup = function () {
+    mousingdown = false;
   }
 }
 /*画图*/
